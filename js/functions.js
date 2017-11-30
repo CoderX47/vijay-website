e0addda77f0ee8be56c531e12dc840c42733104b
CoderX47 <CoderXNk@gmail.com>
2017-11-30 03:42:31 -0800

    DONE

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(window.pageYOffset != 0){
+            $('.navbar-default').addClass('black-header');
+        } else{
+            $('.navbar-default').removeClass('black-header');
+        }
+    });
+});
