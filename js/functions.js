fbeed4414985d01e670cda185bc011ed166c5e8e
CoderX47 <CoderXNk@gmail.com>
2017-12-05 11:15:14 -0800

    Fnl

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(window.pageYOffset != 0){
             $('.navbar-default').addClass('black-header');
+            $('.go-top').fadeIn();
         } else{
             $('.navbar-default').removeClass('black-header');
+            $('.go-top').fadeOut();
         }
     });
 });
